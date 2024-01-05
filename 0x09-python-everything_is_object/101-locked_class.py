e4a3adffb299c627b954bdc506960f409cb18d3b
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-01-05 19:51:28 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,11 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
 class LockedClass:
-    __slots__ = ('first_name',)
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
-    def __setattr__(self, name, value):
-        if not hasattr(self, 'first_name') and name != 'first_name':
-            raise AttributeError(f"Cannot create new instance attribute '{name}'. Only 'first_name' is allowed.")
-        super().__setattr__(name, value)
-
-
-locked_obj = LockedClass()
-locked_obj.first_name = "John"
-print(locked_obj.first_name)
+    __slots__ = ["first_name"]
