87b0ffe7efa29c7f2286f009af2e0b00c369e810
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-01-08 20:23:48 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class Rectangle that inherits from BaseGeometry."""
-from 7-base_geometry import BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
@@ -16,3 +17,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Compute the area of the Rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the string representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
