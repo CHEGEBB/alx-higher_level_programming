5243140bba9c564efee8932f0763184f0f88f889
CHEGEBB <chegephil24@gmail.com>
2024-03-12 07:20:47 +0000

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let language1 = "C is fun";
-let language2 = "Python is cool";
-let language3 = "JavaScript is amazing";
+const language1 = 'C is fun';
+const language2 = 'Python is cool';
+const language3 = 'JavaScript is amazing';
 console.log(language1);
 console.log(language2);
 console.log(language3);
