f2227955e699c574a474103d82f1b3061bc5f7dc
CHEGEBB <chegephil24@gmail.com>
2024-03-12 07:26:28 +0000

    new

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const args = process.argv.slice(2); // Extract arguments, excluding the first two (node binary and script name)
 
 if (args.length === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (args.length === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
