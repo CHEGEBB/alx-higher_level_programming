bc84675b087a7cd6b20fad92dc159d17c32e9b65
CHEGEBB <chegephil24@gmail.com>
2024-03-12 10:33:17 +0300

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 const arg = process.argv[2]; // Extract the first argument passed to the script
 
-if (arg === 0) {
+if (arg === undefined) {
   console.log('No argument');
 } else {
   console.log(arg);
