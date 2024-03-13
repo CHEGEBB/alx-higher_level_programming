6feba54f997fd5734195b142f491c0e20972e4da
CHEGEBB <chegephil24@gmail.com>
2024-03-13 10:32:23 +0300

    update

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,27 @@
 #!/usr/bin/node
-const dict = require('./102-data').dict;
-const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(key);
+const fs = require('fs');
+
+function concatFiles(file1, file2, destination) {
+    try {
+        // Read content of first file
+        const content1 = fs.readFileSync(file1, 'utf8');
+        // Read content of second file
+        const content2 = fs.readFileSync(file2, 'utf8');
+        // Concatenate contents
+        const concatenatedContent = `${content1}${content2}`;
+        // Write concatenated content to destination file
+        fs.writeFileSync(destination, concatenatedContent);
+        console.log(`Concatenation successful. Output written to ${destination}`);
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+    }
 }
-console.log(newDict);
+
+const args = process.argv.slice(2);
+if (args.length !== 3) {
+    console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
+    process.exit(1);
+}
+
+const [file1, file2, destination] = args;
+concatFiles(file1, file2, destination);
