76b531c175c6dc5a3fb6b777b070246ad75627ae
CHEGEBB <chegephil24@gmail.com>
2024-03-27 14:40:09 +0300

    peak2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 
-"""This module finds a peak in a list of unsorted integers.
+"""This module finds peaks in a list of unsorted integers.
 It uses a recursive binary search algorithm to find the peak.
 It achieves efficiency by dividing the list in half and
 searching the half that contains the greater of the two
 """
+
 def find_peak(list_of_integers):
-    """This function finds a peak in a list of unsorted integers"""
+    """This function finds peaks in a list of unsorted integers"""
     if not list_of_integers:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        return max(list_of_integers)
+
     mid = len(list_of_integers) // 2
-    if list_of_integers[mid] > list_of_integers[mid - 1] and \
-       list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if list_of_integers[mid + 1] > list_of_integers[mid]:
-        return find_peak(list_of_integers[mid + 1:])
-    return find_peak(list_of_integers[:mid])
+    peak = list_of_integers[mid]
+
+    # Check if peak is greater than its neighbors
+    if (mid == 0 or peak >= list_of_integers[mid - 1]) and \
+       (mid == len(list_of_integers) - 1 or peak >= list_of_integers[mid + 1]):
+        return peak
+
+    # Recursively search the left half if peak is less than its left neighbor
+    if mid > 0 and peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+
+    # Recursively search the right half if peak is less than its right neighbor
+    return find_peak(list_of_integers[mid + 1:])
