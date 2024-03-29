0c2634e06941f7a0b9ad319592eee5265a6e32e1
CHEGEBB <chegephil24@gmail.com>
2024-03-29 09:54:01 +0300

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
+"""This script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
+It handles HTTP errors.
 """
 import sys
 import requests
