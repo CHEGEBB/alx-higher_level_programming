f9c3e24c265dc3c71aa90b428ae18ac6bcc5b83f
CHEGEBB <chegephil24@gmail.com>
2024-03-29 09:52:35 +0300

    status

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL
+
+"""This script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
+It fetches the URL and displays the value of the X-Request-Id variable found in the header of the response.
 """
 import sys
 import requests
