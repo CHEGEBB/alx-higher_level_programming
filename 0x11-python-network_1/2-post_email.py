a5f2fe34cd1f1a1fa57611e063117747dc2ffa29
CHEGEBB <chegephil24@gmail.com>
2024-03-29 09:49:38 +0300

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" script that takes in a URL and an email, sends a POST request to
-the passed URL with the email as a parameter, and finally displays
-the body of the response. """
+
+""" This script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""
 if __name__ == '__main__':
     import urllib.request
     import urllib.parse
